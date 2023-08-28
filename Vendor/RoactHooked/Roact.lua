847e587e2ed413c5150eeb103c4948008d425c83
howmanysmall <lhamil29@bruinmail.slcc.edu>
2023-08-28 15:45:40 -0600

    fix error

--- Vendor/RoactHooked/Roact.lua
+++ Vendor/RoactHooked/Roact.lua
@@ -1,10 +1,14 @@
 local modules = script:FindFirstAncestor("node_modules")
 
-if modules:FindFirstChild("roact") then
-	return require(modules.roact.src)
-elseif modules:FindFirstChild("@rbxts") then
-	return require(modules["@rbxts"].roact.src)
-elseif script.Parent.Parent:FindFirstChild("Roact") then
+if modules then
+	if modules:FindFirstChild("roact") then
+		return require(modules.roact.src)
+	elseif modules:FindFirstChild("@rbxts") then
+		return require(modules["@rbxts"].roact.src)
+	end
+end
+
+if script.Parent.Parent:FindFirstChild("Roact") then
 	return require(script.Parent.Parent.Roact)
 else
 	error("Could not find Roact or @rbxts/roact in the parent hierarchy.")
